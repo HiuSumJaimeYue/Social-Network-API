a7b55960fd37dcca5a3b1a7fa6f660d3e45ca7d7
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-23 04:06:51 -0400

    Connect thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,12 +2,12 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
     // add thought to user
-    addThought({ params, body }, res) {
+    addThought({ body }, res) {
         console.log(body);
         Thought.create(body)
             .then(({ _id }) => {
                 return User.findOneAndUpdate(
-                    { _id: params.userId },
+                    { _id: body.userId },
                     { $push: { thoughts: _id } },
                     { new: true }
                 );
@@ -21,7 +21,14 @@ const thoughtController = {
             })
             .catch(err => res.json(err));
     },
+    //Add reaction
     addReaction({ params, body }, res) {
+
+        /*
+         { reactionBody: "Reaction 1",
+           username: "NewUser" }
+        */
+
         Thought.findOneAndUpdate(
           { _id: params.thoughtId },
           { $push: { reactions: body } },
@@ -36,7 +43,6 @@ const thoughtController = {
             })
             .catch(err => res.json(err));
     },
-
     // remove thought
     removeThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.thoughtId })
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    addThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router
+    .route('/')
+    // .get(getAllThought)
+    .post(addThought);
+
+// /api/thoughts/<thoughtId>
+router.route('/:thoughtId')
+    // .get(getThoughtById)
+    // .put(updateThought)
+    .delete(removeThought)
+    ;
+
+// /api/thoughts/<thoughtId>/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(removeReaction);
+
+module.exports = router;
